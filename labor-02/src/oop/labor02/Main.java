9b5fbe267c65b52f8a0e1b4c05a7d14580ea3453
sikoruben <79188659+sikoruben@users.noreply.github.com>
2022-03-08 19:32:35 +0200

    push1

--- labor-02/src/oop/labor02/Main.java
+++ labor-02/src/oop/labor02/Main.java
@@ -2,6 +2,12 @@ package oop.labor02;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        BankAccount account1 = new BankAccount("OTP00001");
+        System.out.println(account1.getAccountNumber()+": "+account1.getBalance());
+        account1.deposit(1000);
+        boolean result = account1.withdraw(500);
+        if( !result ){
+            System.out.println("you do not have ");
+        }
     }
 }
